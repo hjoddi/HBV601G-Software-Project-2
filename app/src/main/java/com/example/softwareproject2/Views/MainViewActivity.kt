f2af851689ca9aee2e939806746420ef6c583abc
hjoddi <hjoddi@gmail.com>
2023-01-13 00:42:26 +0000

    MainView javadoc; foundation laid, ready for further dev.

--- app/src/main/java/com/example/softwareproject2/Views/MainViewActivity.kt
+++ app/src/main/java/com/example/softwareproject2/Views/MainViewActivity.kt
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.softwareproject2.R
 
+/**
+ * View for the "front page" of the app; i.e. the first view when the app is opened.
+ */
 class MainViewActivity : AppCompatActivity() {
+    /**
+     * This method is called when the view is activated.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Connect to the layout.
         setContentView(R.layout.activity_main_view)
     }
 }
