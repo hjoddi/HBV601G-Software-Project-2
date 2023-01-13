730ea412d4150d483f7c42aba56840d424b887f9
hjoddi <hjoddi@gmail.com>
2023-01-13 00:36:30 +0000

    initial commit

---
+++ app/src/main/java/com/example/softwareproject2/Views/MainViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.softwareproject2.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.softwareproject2.R
+
+class MainViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_view)
+    }
+}
