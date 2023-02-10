15fc1566dd7f1179f75cf70dd9cb037aa65739fb
hjoddi <hjoddi@gmail.com>
2023-02-10 21:41:46 +0000

    Created constructor for singleton NetworkManager.

--- app/src/main/java/com/example/softwareproject2/Networking/NetworkManager.java
+++ app/src/main/java/com/example/softwareproject2/Networking/NetworkManager.java
@@ -1,4 +1,16 @@
 package com.example.softwareproject2.Networking;
 
 public class NetworkManager {
+
+    private static NetworkManager mInstance;
+
+    public static synchronized NetworkManager getInstance() {
+        if (mInstance == null) {
+            mInstance = new NetworkManager();
+        }
+        return mInstance;
+    }
+
+    private NetworkManager() {
+    }
 }
