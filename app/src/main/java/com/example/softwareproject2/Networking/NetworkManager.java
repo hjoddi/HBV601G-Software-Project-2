c1ffea8627e96f0b96c900616a8d63c05806d81a
hjoddi <hjoddi@gmail.com>
2023-02-10 21:49:21 +0000

    NetworkManager

--- app/src/main/java/com/example/softwareproject2/Networking/NetworkManager.java
+++ app/src/main/java/com/example/softwareproject2/Networking/NetworkManager.java
@@ -1,16 +1,54 @@
 package com.example.softwareproject2.Networking;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Klasi sem hefur einungis það hlutverk að sækja hluti frá networkinu og skila því
+ *  til baka í gegnum callback.
+ */
 public class NetworkManager {
 
+    // Instance variables.
     private static NetworkManager mInstance;
+    private static RequestQueue mQueue;
+    private Context mContext;
 
-    public static synchronized NetworkManager getInstance() {
+    /**
+     * Returns the instance of the singleton NetworkManager, or creates the
+     *  instance if it doesn't already exist.
+     * @param context - The context.
+     * @return - The instance of the singleton NetworkManager.
+     */
+    public static synchronized NetworkManager getInstance(Context context) {
         if (mInstance == null) {
-            mInstance = new NetworkManager();
+            mInstance = new NetworkManager(context);
         }
         return mInstance;
     }
 
-    private NetworkManager() {
+    /**
+     * Constructor for the NetworkManager
+     * @param context - The context.
+     */
+    private NetworkManager(Context context) {
+        mContext = context;
+        mQueue = getRequestQueue();
     }
+
+    /**
+     * Returns the instance of the RequestQueue, or creates it if it doesn't
+     *  already exist.
+     * @return - A RequestQueue.
+     */
+    public RequestQueue getRequestQueue() {
+        if (mQueue == null) {
+            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mQueue;
+    }
+
+
 }
