92d748f4740c4c21c3506ac732cf82563dba6ddf
hjoddi <hjoddi@gmail.com>
2023-02-15 21:53:02 +0000

    Added the Recipe and User objects to the project.

---
+++ app/src/main/java/com/example/softwareproject2/Model/Recipe.java
@@ -0,0 +1,120 @@
+package com.example.softwareproject2.Model;
+
+import java.util.HashSet;
+
+/**
+ * Recipe object. Contains information pertaining to a particular recipe.
+ */
+public class Recipe {
+	/**
+	 * Variables.
+	 */
+
+	private Long id;
+
+	private HashSet<String> ingredients;
+	//@Column(columnDefinition = "TEXT", length = 1000)
+	private String instructions;
+	private String imageName;
+	private String name;
+	private HashSet<String> comments;
+	private User user;
+	private HashSet<Integer> ratings;	// Contains individual ratings.
+	private HashSet<String> raters; 	// Contains usernames of those who have rated.
+	private int rating;	// Recipe's rating. Average of ratings.
+
+	/**
+	 * Getters and setters.
+	 */
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public void addRating(int rating, String username) {
+		System.out.println("DEBUG - addRating()");
+		if (!this.raters.contains(username)) {
+			System.out.println("DEBUG - !this.raters.contains(username)");
+			this.ratings.add(rating);
+			this.raters.add(username);
+		}
+	}
+	public HashSet<String> getIngredients() {
+		return ingredients;
+	}
+	public HashSet<String> getComments() { return comments; }
+	public String getInstructions() {
+		return instructions;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public HashSet<Integer> getRatings() {
+		return ratings;
+	}
+	public HashSet<String> getRaters() {
+		return raters;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	/**
+	 * Constructor for Recipe object.
+	 * @param name - Name of the recipe.
+	 * @param ingredients - Ingredients in the recipe.
+	 * @param instructions - Instructions on how to create the recipe.
+	 */
+	public Recipe(String name, HashSet<String> ingredients, String instructions, String imageName) {
+		this.ingredients = ingredients;
+		this.instructions = instructions;
+		this.name = name;
+		this.rating = 5;
+		this.raters = new HashSet<>();
+		this.ratings = new HashSet<>();
+		this.comments = new HashSet<>();
+		this.imageName = imageName;
+	}
+	/**
+	 * Generic constructor without parameters.
+	 */
+	public Recipe() {}
+
+	/**
+	 * Adds a comment to the recipe.
+	 * @param comment - The comment in String-form.
+	 */
+	public void AddComment(String comment) {
+		this.comments.add(comment);
+	}
+
+	/**
+	 * Describes the recipe in String format.
+	 * @return - String containing the recipe's variables.
+	 */
+	@Override
+	public String toString() {
+		return "Recipe{" +
+				"id=" + id +
+				", ingredients=" + ingredients +
+				", instructions='" + instructions +
+				", comments='" + comments + '\'' +
+				'}';
+	}
+}
---
+++ app/src/main/java/com/example/softwareproject2/Model/User.java
@@ -0,0 +1,87 @@
+package com.example.softwareproject2.Model;
+
+
+import java.util.HashSet;
+
+/**
+ * User object. Contains information pertaining to a particular user.
+ */
+public class User {
+    /**
+     * Variables.
+     */
+    private long ID;
+
+    private String username;
+    private String password;
+
+    //@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private HashSet<Long> favoriteRecipes;
+
+    /**
+     * Constructor without arguments.
+     */
+    public User(){}
+
+    /**
+     * Constructor with arguments.
+     * @param username - Name of the user.
+     * @param password - Password of the user.
+     */
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        favoriteRecipes = new HashSet<>();
+    }
+
+    /**
+     * Getters and setters.
+     */
+    public long getID() {
+        return ID;
+    }
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public HashSet<Long> getFavoriteRecipes() {
+        if(favoriteRecipes == null){
+            favoriteRecipes = new HashSet<>();
+        }
+        return favoriteRecipes;
+    }
+    public void setFavoriteRecipes(HashSet<Long> favoriteRecipes) {
+        this.favoriteRecipes = favoriteRecipes;
+    }
+
+    /**
+     * Adds a recipe to a user's list of favourite recipes.
+     * @param id - Id of recipe to be added to the user's list of favourite recipes.
+     */
+    public void addToFavoriteRecipes(Long id) {
+        if(favoriteRecipes == null){
+            favoriteRecipes = new HashSet<>();
+        }
+        favoriteRecipes.add(id);
+    }
+
+    /**
+     * Removes a recipe from a user's list of favourite recipes.
+     * @param id - Id of recipe to be added to the user's list of favourite recipes.
+     */
+    public void removeFromFavouriteRecipes(Long id) {
+        favoriteRecipes.remove(id);
+    }
+
+}
