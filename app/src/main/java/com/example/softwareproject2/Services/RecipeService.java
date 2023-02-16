6626389e95ea0f9e51e70557126870e433ee47ee
hjoddi <hjoddi@gmail.com>
2023-02-16 20:40:50 +0000

    Added a filteredRecipeList() method to RecipeService, which takes a string and returns a list of recipes matching said string.

--- app/src/main/java/com/example/softwareproject2/Services/RecipeService.java
+++ app/src/main/java/com/example/softwareproject2/Services/RecipeService.java
@@ -2,6 +2,9 @@ package com.example.softwareproject2.Services;
 
 import com.example.softwareproject2.Model.Recipe;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -13,8 +16,41 @@ public class RecipeService {
     // Instance variables.
     private List<Recipe> mRecipeBank;
 
+    /**
+     * Constructor.
+     */
     public RecipeService() {
         TempBackendProvider TBP = TempBackendProvider.INSTANCE;
         this.mRecipeBank = TBP.getRecipes();
+    }
+
+    /**
+     * Returns a list of recipes whose name or ingredients match the filter string.
+     * @param filter - The string which should match a recipe's name or ingredients.
+     * @return - List of recipes whose name or ingredients match the filter string.
+     */
+    public List<Recipe> filteredRecipeList(String filter) {
+        ArrayList<Recipe> filteredRecipes = new ArrayList<>();
+
+        for (int i = 0; i < mRecipeBank.size(); i++) {
+            Recipe currentRecipe = mRecipeBank.get(i);
+            HashSet<String> currentRecipeIngredients = currentRecipe.getIngredients();
+            Iterator<String> currentRecipeIngredientsIterator = currentRecipeIngredients.iterator();
+
+            if (currentRecipe.getName().contains(filter)) {
+                filteredRecipes.add(currentRecipe);
+            }
+            else {
+                while (currentRecipeIngredientsIterator.hasNext()) {
+                    String ingredient = currentRecipeIngredientsIterator.next();
+
+                    if (ingredient.contains(filter)) {
+                        filteredRecipes.add(currentRecipe);
+                    }
+                }
+            }
+        }
+
+        return filteredRecipes;
     }
 }
