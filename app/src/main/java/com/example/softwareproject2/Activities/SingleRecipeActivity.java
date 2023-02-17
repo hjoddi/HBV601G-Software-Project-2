8b0fe8128702952c2d988ff760e9ef3ddb181328
hjoddi <hjoddi@gmail.com>
2023-02-17 02:47:58 +0000

    SingleRecipeActivity back button.

--- app/src/main/java/com/example/softwareproject2/Activities/SingleRecipeActivity.java
+++ app/src/main/java/com/example/softwareproject2/Activities/SingleRecipeActivity.java
@@ -3,6 +3,9 @@ package com.example.softwareproject2.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.softwareproject2.Model.Recipe;
 import com.example.softwareproject2.R;
@@ -15,6 +18,8 @@ public class SingleRecipeActivity extends AppCompatActivity {
 
     // Instance variables.
     private Recipe recipe;
+    private ListView mListView;
+    private Button mBtnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,22 @@ public class SingleRecipeActivity extends AppCompatActivity {
 
         // Access bundled extras.
         recipe = (Recipe) getIntent().getSerializableExtra("recipe");
+
+        // Connect UI widgets.
+        mListView = findViewById(R.id.singleRecipeActivityListView);
+        mBtnBack = findViewById(R.id.singleRecipeActivityBtnBack);
+
+        // Establish widget functionalities.
+        mBtnBack.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Closes this activity, returning to the search
+             *  result view.
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
