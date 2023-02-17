1aa4598806af5d0a76150b01fcd4128dd9329f7e
hjoddi <hjoddi@gmail.com>
2023-02-17 02:37:35 +0000

    javadoc

--- app/src/main/java/com/example/softwareproject2/Activities/SingleRecipeActivity.java
+++ app/src/main/java/com/example/softwareproject2/Activities/SingleRecipeActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.softwareproject2.Model.Recipe;
 import com.example.softwareproject2.R;
 
+/**
+ * This class manages the view displaying information about a specific
+ *  recipe.
+ */
 public class SingleRecipeActivity extends AppCompatActivity {
+
+    // Instance variables.
+    private Recipe recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Connect to layout.
         setContentView(R.layout.activity_single_recipe);
+
+        // Access bundled extras.
+        recipe = (Recipe) getIntent().getSerializableExtra("recipe");
     }
 }
