eb6285916d974eaf75d7a1effd2b4067e9513ea8
hjoddi <hjoddi@gmail.com>
2023-04-05 23:17:27 +0000

    Buttons komnir, impl ekki komið nema main menu.

--- app/src/main/java/com/example/softwareproject2/Activities/AdminActivity.java
+++ app/src/main/java/com/example/softwareproject2/Activities/AdminActivity.java
@@ -3,14 +3,37 @@ package com.example.softwareproject2.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.softwareproject2.R;
+import com.example.softwareproject2.Services.BackendSingleton;
 
 public class AdminActivity extends AppCompatActivity {
 
+    // Instance variables.
+    private BackendSingleton mBackend;
+    private Button mBtnDeleteUsers, mBtnDeleteRecipes, mBtnMainMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Connect to layout.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        // Initialize variables.
+        mBackend = BackendSingleton.getInstance();
+        mBtnDeleteUsers = findViewById(R.id.adminActivityBtnDeleteUsers);
+        mBtnDeleteRecipes = findViewById(R.id.adminActivityBtnDeleteRecipes);
+        mBtnMainMenu = findViewById(R.id.adminActivityBtnMainMenu);
+
+        // Button functionalities.
+        mBtnMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
