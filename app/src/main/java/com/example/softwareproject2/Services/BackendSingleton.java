bfa32f7f775b83339bc871bd62d0b93522814bc0
hjoddi <hjoddi@gmail.com>
2023-04-05 23:28:10 +0000

    Færði það að búa til pre-made recipes úr getRecipes fallinu yfir í constructorinn á BackendSingleton. Nauðsynlegt til að implementa delete recipes.

--- app/src/main/java/com/example/softwareproject2/Services/BackendSingleton.java
+++ app/src/main/java/com/example/softwareproject2/Services/BackendSingleton.java
@@ -31,6 +31,9 @@ public class BackendSingleton {
         if (recipesList == null) {
             recipesList = new ArrayList<>();
         }
+        if (recipesList.isEmpty()) {
+            recipesList = getPreMadeRecipeBank();
+        }
         adminUsername = "admin";
         adminPassword = "admin";
     }
@@ -65,63 +68,71 @@ public class BackendSingleton {
      * @return - recipes: List of all recipes.
      */
     public List<Recipe> getRecipes() {
+        return recipesList;
+    }
 
-        if (recipesList.isEmpty()) {
-            // Populate list.
-            HashSet<String> ingredients = new HashSet<>(); ingredients.add("Rice"); ingredients.add("Milk"); ingredients.add("Raisins");
-            Recipe r1 = new Recipe("Porridge", ingredients, "Add small amount of water and bring it to a boil. " +
-                    "Then add the rice and milk and let it cook at low heat. Add raisings once it starts getting thicker.", "porridge");
-            r1.setId(1L);
-            HashSet<String> ingredients2 = new HashSet<>(); ingredients2.add("Minced-Meat"); ingredients2.add("Spaghetti"); ingredients2.add("Pasta-Sauce"); ingredients2.add("Vegetable");
-            Recipe r2 = new Recipe("Spaghetti", ingredients2, "Boil spaghetti. Cook the meat until brown and add sauce. Add vegetables if desired." +
-                    " Combine.","spaghetti");
-            r2.setId(2L);
-            HashSet<String> ingredients3 = new HashSet<>(); ingredients3.add("Tuna"); ingredients3.add("Pasta"); ingredients3.add("Egg"); ingredients3.add("Vegetable");
-            Recipe r3 = new Recipe("Tuna-Egg-Pasta", ingredients3, "Boil eggs and pasta for 10 minutes. Combine in a bowl and add tuna and vegetables.", "tuna_egg_pasta");
-            r3.setId(3L);
-            HashSet<String> ingredients4 = new HashSet<>(); ingredients4.add("Noodles"); ingredients4.add("Cheese"); ingredients4.add("Egg");
-            Recipe r4 = new Recipe("Slightly better noodles", ingredients4, "Fry an egg. Put noodles and sauce in a pan. " +
-                    "Sprinkle cheese on top and let it melt. Put egg on top.", "slightly_better_noodles");
-            r4.setId(4L);
-            HashSet<String> ingredients5 = new HashSet<>(Arrays.asList("Leftover", "Egg", "Vegetable"));
-            Recipe r5 = new Recipe("Leftover omelette", ingredients5,
-                    "Mix eggs in a bowl. Fry in a pan on low heat. When bottom of egg starts to cook add any vegetables and leftovers. Fold the base into a half moon shape.",
-                    "leftoveromelette");
-            r5.setId(5L);
-            HashSet<String> ingredients6 = new HashSet<>(Arrays.asList("Chicken-Breast", "Peanuts", "Noodles", "Vegetable", "Bean-Sauce"));
-            Recipe r6 = new Recipe("Chicken Noodles stir-fry", ingredients6, "Grind up the peanuts. " +
-                    "Cook chicken in a pan with bean sauce. " +
-                    "Boil the noodles and add vegetables to the pot. " +
-                    "Mix everything with peanuts on top.",
-                    "chicken_noodles_stir_fry");
-            r6.setId(6L);
-            HashSet<String> ingredients7= new HashSet<>(Arrays.asList("Spaghetti", "Cheese", "Garlic", "Olive-Oil"));
-            Recipe r7 = new Recipe("Spaghetti aglio e olio", ingredients7, "Cook pasta. Cook garlic in pan with olive oil until golden. Mix pasta with garlic in pan." +
-                    " Add any spices or herbs.",
-                    "aglio_e_olio");
-            r7.setId(7L);
-            HashSet<String> ingredients8= new HashSet<>(Arrays.asList("Pasta", "Garlic", "Mushroom", "Cheese", "Creme-Fraiche"));
-            Recipe r8 = new Recipe("Mushroom Garlic Pasta", ingredients8, "Cook pasta. Fry garlic and mushrooms. Put pasta into pan and mix in cheese and" +
-                    " creme fraiche.",
-                    "mushroom_pasta");
-            r8.setId(8L);
+    /**
+     * Creates a list of pre-made recipes.
+     * @return List of pre-made recipes.
+     */
+    public ArrayList<Recipe> getPreMadeRecipeBank() {
 
-            // TODO: remove this - Add a comment for DEV purposes.
-            HashSet<String> r8Comments = new HashSet<>();
-            r8Comments.add("Fantastic!");
-            r8.setComments(r8Comments);
+        // Create recipes.
+        HashSet<String> ingredients = new HashSet<>(); ingredients.add("Rice"); ingredients.add("Milk"); ingredients.add("Raisins");
+        Recipe r1 = new Recipe("Porridge", ingredients, "Add small amount of water and bring it to a boil. " +
+                "Then add the rice and milk and let it cook at low heat. Add raisings once it starts getting thicker.", "porridge");
+        r1.setId(1L);
+        HashSet<String> ingredients2 = new HashSet<>(); ingredients2.add("Minced-Meat"); ingredients2.add("Spaghetti"); ingredients2.add("Pasta-Sauce"); ingredients2.add("Vegetable");
+        Recipe r2 = new Recipe("Spaghetti", ingredients2, "Boil spaghetti. Cook the meat until brown and add sauce. Add vegetables if desired." +
+                " Combine.","spaghetti");
+        r2.setId(2L);
+        HashSet<String> ingredients3 = new HashSet<>(); ingredients3.add("Tuna"); ingredients3.add("Pasta"); ingredients3.add("Egg"); ingredients3.add("Vegetable");
+        Recipe r3 = new Recipe("Tuna-Egg-Pasta", ingredients3, "Boil eggs and pasta for 10 minutes. Combine in a bowl and add tuna and vegetables.", "tuna_egg_pasta");
+        r3.setId(3L);
+        HashSet<String> ingredients4 = new HashSet<>(); ingredients4.add("Noodles"); ingredients4.add("Cheese"); ingredients4.add("Egg");
+        Recipe r4 = new Recipe("Slightly better noodles", ingredients4, "Fry an egg. Put noodles and sauce in a pan. " +
+                "Sprinkle cheese on top and let it melt. Put egg on top.", "slightly_better_noodles");
+        r4.setId(4L);
+        HashSet<String> ingredients5 = new HashSet<>(Arrays.asList("Leftover", "Egg", "Vegetable"));
+        Recipe r5 = new Recipe("Leftover omelette", ingredients5,
+                "Mix eggs in a bowl. Fry in a pan on low heat. When bottom of egg starts to cook add any vegetables and leftovers. Fold the base into a half moon shape.",
+                "leftoveromelette");
+        r5.setId(5L);
+        HashSet<String> ingredients6 = new HashSet<>(Arrays.asList("Chicken-Breast", "Peanuts", "Noodles", "Vegetable", "Bean-Sauce"));
+        Recipe r6 = new Recipe("Chicken Noodles stir-fry", ingredients6, "Grind up the peanuts. " +
+                "Cook chicken in a pan with bean sauce. " +
+                "Boil the noodles and add vegetables to the pot. " +
+                "Mix everything with peanuts on top.",
+                "chicken_noodles_stir_fry");
+        r6.setId(6L);
+        HashSet<String> ingredients7= new HashSet<>(Arrays.asList("Spaghetti", "Cheese", "Garlic", "Olive-Oil"));
+        Recipe r7 = new Recipe("Spaghetti aglio e olio", ingredients7, "Cook pasta. Cook garlic in pan with olive oil until golden. Mix pasta with garlic in pan." +
+                " Add any spices or herbs.",
+                "aglio_e_olio");
+        r7.setId(7L);
+        HashSet<String> ingredients8= new HashSet<>(Arrays.asList("Pasta", "Garlic", "Mushroom", "Cheese", "Creme-Fraiche"));
+        Recipe r8 = new Recipe("Mushroom Garlic Pasta", ingredients8, "Cook pasta. Fry garlic and mushrooms. Put pasta into pan and mix in cheese and" +
+                " creme fraiche.",
+                "mushroom_pasta");
+        r8.setId(8L);
 
-            recipesList.add(r1);
-            recipesList.add(r2);
-            recipesList.add(r3);
-            recipesList.add(r4);
-            recipesList.add(r5);
-            recipesList.add(r6);
-            recipesList.add(r7);
-            recipesList.add(r8);
-        }
-        List<Recipe> recipes = recipesList;
-        return recipes;
+        // TODO: remove this - Add a comment for DEV purposes.
+        HashSet<String> r8Comments = new HashSet<>();
+        r8Comments.add("Fantastic!");
+        r8.setComments(r8Comments);
+
+        // Create a list of the new premade recipes.
+        ArrayList<Recipe> preMadeRecipes = new ArrayList<>();
+        preMadeRecipes.add(r1);
+        preMadeRecipes.add(r2);
+        preMadeRecipes.add(r3);
+        preMadeRecipes.add(r4);
+        preMadeRecipes.add(r5);
+        preMadeRecipes.add(r6);
+        preMadeRecipes.add(r7);
+        preMadeRecipes.add(r8);
+
+        return preMadeRecipes;
     }
 
 
@@ -226,5 +237,12 @@ public class BackendSingleton {
      */
     public void addComment(Recipe recipe, String comment) {
         recipe.AddComment(comment);
+    }
+
+    /**
+     * Deletes all recipes.
+     */
+    public void deleteRecipes() {
+
     }
 }
