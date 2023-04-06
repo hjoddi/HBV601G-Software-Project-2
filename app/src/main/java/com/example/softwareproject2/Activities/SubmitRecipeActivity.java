02c964067ee3159020bddff549fa17d624ae5004
hjoddi <hjoddi@gmail.com>
2023-04-06 23:20:28 +0000

    submit image

--- app/src/main/java/com/example/softwareproject2/Activities/SubmitRecipeActivity.java
+++ app/src/main/java/com/example/softwareproject2/Activities/SubmitRecipeActivity.java
@@ -1,11 +1,17 @@
 package com.example.softwareproject2.Activities;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.softwareproject2.R;
@@ -13,9 +19,24 @@ import com.example.softwareproject2.R;
 public class SubmitRecipeActivity extends AppCompatActivity {
 
     // Instance variables.
-    private Button mBtnIngredientAdd, mBtnIngredientRemove, mBtnBack;
+    private Button mBtnIngredientAdd, mBtnIngredientRemove, mBtnBack, mBtnSelectImage;
     private LinearLayout mIngredientsLayout;
+    private ImageView mImageView;
     private int numEditTexts;   // Number of dynamically created EditTexts.
+    private int selectPicture;  // Constant for comparing activity result code.
+
+    // Required code for submitting an image.
+    private final ActivityResultLauncher<String> imagePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if (result != null) {
+                        mImageView.setImageURI(result);
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +50,11 @@ public class SubmitRecipeActivity extends AppCompatActivity {
         mBtnIngredientRemove = findViewById(R.id.submitRecipeActivityBtnRemove);
         mIngredientsLayout = findViewById(R.id.submitRecipeActivityIngredientLayout);
         numEditTexts = 0;
+        selectPicture = 200;
         mBtnBack = findViewById(R.id.submitRecipeActivityBtnBack);
+        mBtnSelectImage = findViewById(R.id.submitRecipeActivityBtnSelectImage);
+        mImageView = findViewById(R.id.submitRecipeActivityImageView);
+
 
         // Implement button functionalities.
         mBtnIngredientAdd.setOnClickListener(new View.OnClickListener() {
@@ -58,9 +83,13 @@ public class SubmitRecipeActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
-
-
+        mBtnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePickerLauncher.launch("image/*");
+            }
+        });
     }
+
+
 }
